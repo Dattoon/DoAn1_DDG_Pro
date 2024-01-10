cbb331319c6b493d5cedeab80cf963ff7560c925
Dattoon <ttmdvhd@gmail.com>
2024-01-10 18:28:57 +0700

    FIX

---
+++ Areas/Admin/Controllers/ManagerController.cs
@@ -0,0 +1,28 @@
+﻿using DoAn1_DDG_Pro.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn1_DDG_Pro.Areas.Admin.Controllers
+{
+    [Authentication]
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/manager")]
+    public class ManagerController : Controller
+    {
+        [Route("DoanhThu")]
+        public IActionResult DoanhThu()
+        {
+            return View();
+        }
+        [Route("NhanVien")]
+        public IActionResult NhanVien()
+        {
+            return View();
+        }
+        [Route("KhachHang")]
+        public IActionResult KhachHang()
+        {
+            return View();
+        }
+    }
+}
